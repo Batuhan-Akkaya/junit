037fcf641e990b5855353a7a27e4177f1c8d1e8b
bprlab <bprlab@bprlab-iMac-44.local>
2019-03-16 14:05:18 +0300

    files added

---
+++ src/main/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+	public int topla(int sayi1, int say,2) {
+		return sayi1 + sayi2;
+	}
+	
+	public int faktoriyel(int sayi) {
+		int sonuc = 0;
+		for (int i = 1; i <= sayi ) {
+			sonuc *= i;
+		}
+		return sonuc;
+	}
+}
